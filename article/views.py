984a853fe51afa17cb1b781fe10189ac4defbf99
MtKt <i@metakata.one>
2017-03-30 23:14:39 +0800

    use base.html

--- article/views.py
+++ article/views.py
@@ -11,7 +11,7 @@ from django.db.models import Q
 
 class IndexView(ListView):
     model = Article
-    template_name = 'article/index.html'
+    template_name = 'article/index_base.html'
     context_object_name = 'article_list'
     paginate_by = 5
     def get_queryset(self):
@@ -24,11 +24,12 @@ def ArticleView(request, slug, article_id):
 
 class TimeLine(ListView):
     model = Article
-    template_name = 'article/timeline.html'
-    context_object_name = 'article_list'
+    template_name = "article/time.html"
+    context_object_name = "article_list"
     paginate_by = 25
+
     def get_queryset(self):
-        article_list = Article.objects.filter(article_status=0).order_by("-pub_date")
+        article_list = Article.objects.set_all()
         return article_list
 
 class CategoryView(ListView):
@@ -38,8 +39,7 @@ class CategoryView(ListView):
     def get_queryset(self):
         category = self.kwargs.get('category', '')
         try:
-            article_list = \
-                Category.objects.get(name=category).article_set.all()
+            article_list = Category.objects.get(name=category).article_set.all()
         except Category.DoesNotExist:
             logger.error(u'[CategoryView]此分类不存在:[%s]' % category)
             raise Http404
