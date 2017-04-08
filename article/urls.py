0114c8b608dda9cd6c01d4807e42355bfd8c757b
MtKt <i@metakata.one>
2017-04-08 15:21:07 +0800

    add catrgory_view

--- article/urls.py
+++ article/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
                 url(r'^(?P<slug>[-\w\d]+),(?P<article_id>\d+)/$', views.ArticleView.as_view(), name='article'),
                 url(r'^about/$', views.AboutView.as_view(), name = 'about' ),
                 url(r'^timeline/$', views.TimeLine.as_view(), name = 'timeline'),
-                url(r'^category/(?P<category>\w+)/$', views.CategoryView.as_view(), name = 'category'),
+                url(r'^category/$', views.CategoryView.as_view(),name = 'category'),
                 url(r'^cyuuni/$', views.cyuuni_detail, name = 'cyuuni'),
-                ]
+                #url(r'^cyuuni/(?P<cyuuni_id>\d+)/$', views.CyuuniDetail.as_view(), name = 'cyuuni_s'),
+              ]
--- article/views.py
+++ article/views.py
@@ -3,7 +3,7 @@ from math import ceil
 from os.path import join
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
-from .models import Article,Cyuuni
+from .models import Article,Cyuuni,Category
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView, DetailView, TemplateView
 from django.db.models import Q
@@ -58,28 +58,34 @@ class TimeLine(BaseMixIn,TemplateView):
 #    year_all = defaultdict(list)
 #    for article in article_list:
 #       year = article.pub_date.year
-#        year_all[year].append(article)
+#       year_all[year].append(article)
 #    year_all=sorted(year_all.items(),reverse=True)
 #    args = {'year_all':year_all,'article_list':article_list}
 #    return render(request, 'article/timeline.html', args)
 
-class CategoryView(BaseMixIn,ListView):
-    template_name = 'article/category`.html'
+class CategoryView(BaseMixIn,TemplateView):
+    template_name = 'article/category.html'
     def get_context_data(self,**kwargs):
-        model = Article
         article_list = Article.objects.filter(article_status=0).order_by("-pub_date")
-        year_all = defaultdict(list)
+        category_f = defaultdict(list)
         for article in article_list:
-            year = article.pub_date.year
-            year_all[year].append(article)
-        year_all=sorted(year_all.items(),reverse=True)
-        context = super(TimeLine,self).get_context_data(**kwargs)
-        context['year_all'] = year_all
-        context['article_list'] = article_list
+            category = article.category
+            category_f[category].append(article)
+        category_f=sorted(category_f.items(),reverse=True)
+        context = super(CategoryView,self).get_context_data(**kwargs)
+        context['category_f'] = category_f
         return context
+
 
 class AboutView(BaseMixIn,TemplateView):
     template_name = 'article/about.html'
+
+class CyuuniDetail(BaseMixIn,DetailView):
+    template_name = 'article/cyuuni.html'
+    model = Cyuuni
+    context_object_name = 'cyuuni_byou'
+    pk_url_kwarg = 'cyuuni_id'
+
 
 def cyuuni_detail(request, *cyuuni_id):
     cyuuni_byou = Cyuuni.objects.filter(status=0).order_by('?').first()
